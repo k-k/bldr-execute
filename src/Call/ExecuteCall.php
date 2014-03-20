7024e4daa03f544ba82ca49cb4b65c7f57a05d88
Aaron Scherer <aequasi@gmail.com>
2014-03-20 09:20:58 -0700

    Updating definitions

---
+++ src/Call/ExecuteCall.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of Bldr.io
+ *
+ * (c) Aaron Scherer <aequasi@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace Bldr\Extension\Execute\Service;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Aaron Scherer <aaron@undergroundelephant.com>
+ */
+class ExecuteCall implements \Bldr\Call\CallInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function run(OutputInterface $output, array $arguments)
+    {
+        $output->writeln(["", print_r($arguments, true), ""]);
+    }
+}
--- src/DependencyInjection/ExecuteExtension.php
+++ src/DependencyInjection/ExecuteExtension.php
@@ -13,6 +13,8 @@ namespace Bldr\Extension\Execute\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
 /**
  * @author Aaron Scherer <aaron@undergroundelephant.com>
@@ -31,6 +33,7 @@ class ExecuteExtension extends Extension
      */
     public function load(array $config, ContainerBuilder $container)
     {
-        
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/Resources/config/'));
+        $loader->load('services.yml');
     }
 }
