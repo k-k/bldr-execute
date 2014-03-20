fab81662951fa74c1257481539e6cfcb695560f7
Aaron Scherer <aequasi@gmail.com>
2014-03-20 09:36:55 -0700

    Updating Call logic

--- src/Call/ExecuteCall.php
+++ src/Call/ExecuteCall.php
@@ -20,9 +20,33 @@ class ExecuteCall implements \Bldr\Call\CallInterface
 {
     /**
      * {@inheritDoc}
+     *
+     * Logic obtained from http://stackoverflow.com/a/6144213/248903
      */
     public function run(OutputInterface $output, array $arguments)
     {
-        $output->writeln(["", print_r($arguments, true), ""]);
+        $command = '';
+
+        foreach ($arguments as $argument) {
+            $command .= $argument . ' ';
+        }
+
+        ob_implicit_flush(true);
+        ob_end_flush();
+        flush();
+
+        $descriptorspec = array(
+            0 => array("pipe", "r"), // stdin is a pipe that the child will read from
+            1 => array("pipe", "w"), // stdout is a pipe that the child will write to
+            2 => array("pipe", "w") // stderr is a pipe that the child will write to
+        );
+        $pipes = [];
+
+        $process = proc_open($command, $descriptorspec, $pipes);
+        if (is_resource($process)) {
+            while ($s = fgets($pipes[1])) {
+                $output->writeln($s);
+            }
+        }
     }
 }
