773c7e190d465c859509382936f417940b184250
Aaron Scherer <aequasi@gmail.com>
2014-03-23 14:01:09 -0700

    Adding tokenizer

--- src/Call/ExecuteCall.php
+++ src/Call/ExecuteCall.php
@@ -11,9 +11,9 @@
 
 namespace Bldr\Extension\Execute\Call;
 
+use Bldr\Application;
 use Symfony\Component\Console\Helper\FormatterHelper;
 use Symfony\Component\Console\Output\StreamOutput;
-use Symfony\Component\Process\Process;
 use Symfony\Component\Process\ProcessBuilder;
 
 /**
@@ -29,15 +29,23 @@ class ExecuteCall extends \Bldr\Call\AbstractCall
     public function run(array $arguments)
     {
         if ($this->call->has('output')) {
-            $append = $this->call->has('append') && $this->call->append ? 'a' : 'w';
-            $stream = fopen($this->call->output, $append);
+            $append       = $this->call->has('append') && $this->call->append ? 'a' : 'w';
+            $stream       = fopen($this->call->output, $append);
             $this->output = new StreamOutput($stream, StreamOutput::VERBOSITY_NORMAL, true);
         }
 
         /** @var FormatterHelper $formatter */
         $formatter = $this->helperSet->get('formatter');
 
+
+        $this->findTokens($arguments);
+
         $builder = new ProcessBuilder($arguments);
+
+        if ($this->call->has('cwd')) {
+            $builder->setWorkingDirectory($this->call->cwd);
+        }
+
         $process = $builder->getProcess();
 
         if (get_class($this) === 'Bldr\Extension\Execute\Call\ExecuteCall') {
@@ -56,8 +64,46 @@ class ExecuteCall extends \Bldr\Call\AbstractCall
             }
         );
 
-        if ($this->failOnError && !in_array($process->getExitCode(), $this->successStatusCodes)) {
-            throw new \Exception("Failed on the {$this->task->getName()} task.\n" . $process->getErrorOutput());
+        if ($this->call->has('failOnError') && $this->call->failOnError) {
+            if ($this->call->has('successCodes') && !in_array($process->getExitCode(), $this->call->successCodes)) {
+                throw new \Exception("Failed on the {$this->task->getName()} task.\n" . $process->getErrorOutput());
+            }
         }
+    }
+
+    /**
+     * Runs the tokenizer on all the arguments
+     *
+     * @param string[] $arguments
+     */
+    private function findTokens(array &$arguments)
+    {
+        foreach ($arguments as $index => $argument) {
+            $arguments[$index] = $this->replaceTokens($argument);
+        }
+    }
+
+    /**
+     * Tokenizes the given string
+     *
+     * @param string $argument
+     *
+     * @return string
+     */
+    private function replaceTokens($argument)
+    {
+        $token_format = '/\$(.+)\$/';
+
+        preg_match_all($token_format, $argument, $matches, PREG_SET_ORDER);
+
+        if (sizeof($matches) < 1) {
+            return $argument;
+        }
+
+        foreach ($matches as $match) {
+            $argument = str_replace($match[0], Application::$$match[1], $argument);
+        }
+
+        return $argument;
     }
 }
